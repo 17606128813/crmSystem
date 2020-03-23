9ef5e9e832d45f873b664978bd73305fe520b977
17606128813 <1739945072@qq.com>
2020-03-23 22:57:03 +0800

    机构级别表

---
+++ CRMSystem/Model/Manage/RankType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Manage
+{
+    /// <summary>
+    /// 机构级别表
+    /// </summary>
+    public class RankType
+    {
+        /// <summary>
+        /// 编号
+        /// </summary>
+        public int RId { get; set; }
+        /// <summary>
+        /// 级别名称
+        /// </summary>
+        public string RName { get; set; }
+        /// <summary>
+        /// 状态
+        /// </summary>
+        public int RState { get; set; }
+    }
+}
