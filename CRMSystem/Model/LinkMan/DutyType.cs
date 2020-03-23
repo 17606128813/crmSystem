126b85db7a978d59494f8096a538e01ff2c176a0
17606128813 <1739945072@qq.com>
2020-03-23 22:54:30 +0800

    职务表

---
+++ CRMSystem/Model/LinkMan/DutyType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.LinkMan
+{
+    /// <summary>
+    /// 职务表
+    /// </summary>
+    public class DutyType
+    {
+        /// <summary>
+        /// 编号
+        /// </summary>
+        public int DId { get; set; }
+        /// <summary>
+        /// 职务名称
+        /// </summary>
+        public string DName { get; set; }
+        /// <summary>
+        /// 状态
+        /// </summary>
+        public int DState { get; set; }
+    }
+}
