a97b03b8f5347fdfebdbdd3e4208acb7f21b2389
17606128813 <1739945072@qq.com>
2020-03-23 22:54:55 +0800

    等级表

---
+++ CRMSystem/Model/LinkMan/GradeType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.LinkMan
+{
+    /// <summary>
+    /// 等级表
+    /// </summary>
+    public class GradeType
+    {
+        /// <summary>
+        /// 编号
+        /// </summary>
+        public int GId { get; set; }
+        /// <summary>
+        /// 等级名称
+        /// </summary>
+        public string GName { get; set; }
+        /// <summary>
+        /// 状态
+        /// </summary>
+        public int GState { get; set; }
+    }
+}
