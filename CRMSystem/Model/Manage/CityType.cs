8d4da378f7f1f02d7c591d2891c6d6500f30dea8
17606128813 <1739945072@qq.com>
2020-03-23 22:55:59 +0800

    省份表

---
+++ CRMSystem/Model/Manage/CityType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Manage
+{
+    /// <summary>
+    /// 省份表
+    /// </summary>
+    public class CityType
+    {
+        /// <summary>
+        /// 编号
+        /// </summary>
+        public int CId { get; set; }
+        /// <summary>
+        /// 省份名称
+        /// </summary>
+        public string CName { get; set; }
+        /// <summary>
+        /// 状态
+        /// </summary>
+        public int CState { get; set; }
+    }
+}
